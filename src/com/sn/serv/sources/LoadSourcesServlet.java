103a2ca9429f5650b714a1354fc1f098d3240e71
Will <wgrosmon@uncc.edu>
2017-04-08 21:02:42 -0400

    Add sources servlet utilities

---
+++ src/com/sn/serv/sources/LoadSourcesServlet.java
@@ -0,0 +1,47 @@
+package com.sn.serv.sources;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sn.database.objects.Source;
+
+/**
+ * Servlet implementation class LoadSourcesServlet
+ */
+@WebServlet("/LoadSourcesServlet")
+public class LoadSourcesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoadSourcesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<Source> sourcesList = null;
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
