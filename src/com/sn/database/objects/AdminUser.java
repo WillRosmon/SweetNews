5f9bf8c3a916f586c31b7ac6d0b177518c7cabb9
Will <wgrosmon@uncc.edu>
2017-04-17 19:56:34 -0400

    Add AdminUser

---
+++ src/com/sn/database/objects/AdminUser.java
@@ -0,0 +1,18 @@
+package com.sn.database.objects;
+
+public class AdminUser extends User {
+
+	public Source createSource(Source source) {
+		//insert news source to DB
+		return null;
+	}
+	
+	public Article createArticle(Article article) {
+		//insert article to DB
+		return null;
+	}
+	
+	public void approveSource(Source source) {
+		//update DB with approval
+	}
+}
