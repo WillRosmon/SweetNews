409e722cde59e24c9553e47f8da1f72671d0fa05
Will <wgrosmon@uncc.edu>
2017-04-22 19:21:10 -0400

    add asArticle method

--- src/com/sn/database/accessors/ArticleAccessor.java
+++ src/com/sn/database/accessors/ArticleAccessor.java
@@ -2,10 +2,12 @@ package com.sn.database.accessors;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import com.sn.database.objects.Article;
+import com.sn.database.utilities.DbConstants;
 
 public class ArticleAccessor {
 
@@ -24,6 +26,16 @@ public class ArticleAccessor {
 		return null;
 	}
 	
+	private Article asArticle(ResultSet rs) throws SQLException {
+		Article article = new Article();
+		article.setAuthor(rs.getString(DbConstants.ARTICLE_COL_AUTHOR));
+		article.setDescription(rs.getString(DbConstants.ARTICLE_COL_DESCRIPTION));
+		article.setPublishTime(rs.getString(DbConstants.ARTICLE_COL_PUBLISHTIME));
+		article.setTitle(rs.getString(DbConstants.ARTICLE_COL_TITLE));
+		article.setUrl(rs.getString(DbConstants.ARTICLE_COL_URL));
+		article.setUrlToImage(rs.getString(DbConstants.ARTICLE_COL_URLTOIMAGE));
+		return article;
+	}
 	
 	private PreparedStatement getInsertArticleStatement() throws SQLException{
 		StringBuilder sb = new StringBuilder();
