354e0a58cccff05beeed0f92a295a265aa24ff74
Will <wgrosmon@uncc.edu>
2017-04-22 13:21:18 -0400

    Add functionality to SourceAccessor

--- src/com/sn/database/accessors/SourceAccessor.java
+++ src/com/sn/database/accessors/SourceAccessor.java
@@ -13,9 +13,19 @@ import com.sn.database.utilities.DbConstants;
 import com.sn.database.utilities.DbUtil;
 
 public class SourceAccessor {
-	Connection connection = null;
+	private Connection connection = null;
 	
-	public static List<Source> selectAllSources() {
+	private PreparedStatement _insertSource;
+	private PreparedStatement _selectourceByID;
+	private PreparedStatement _selectSourcesByTopic;
+	private PreparedStatement _selectAllSources;
+	private PreparedStatement _deleteSource;
+	
+	public SourceAccessor(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public List<Source> selectAllSources() {
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection connection = pool.getConnection();
 		
@@ -44,7 +54,7 @@ public class SourceAccessor {
 	}
 	
 	
-	private static void cleanup(PreparedStatement ps, ResultSet rs) {
+	private void cleanup(PreparedStatement ps, ResultSet rs) {
 		if(ps != null) {
 			DbUtil.closePreparedStatement(ps);
 		}
@@ -52,9 +62,46 @@ public class SourceAccessor {
 			DbUtil.closeResultSet(rs);
 		}
 	}
-	private static Source asSource(ResultSet rs) {
+	private Source asSource(ResultSet rs) {
 		Source source = new Source();
 		
 		return source;
+	}
+	
+	private PreparedStatement getInsertStatement() throws SQLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO ");
+		sb.append(DbConstants.SOURCE_TABLE);
+		
+		_insertSource = connection.prepareStatement(sb.toString());
+		return _insertSource;
+	}
+	
+	private PreparedStatement getSelectAllSourcesStatement() throws SQLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT * FROM ");
+		sb.append(DbConstants.SOURCE_TABLE);
+		_selectAllSources = connection.prepareStatement(sb.toString());
+		return _selectAllSources;
+	}
+	
+	private PreparedStatement getSelectSourceByIDStatement() throws SQLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT * FROM ");
+		sb.append(DbConstants.SOURCE_TABLE);
+		sb.append(" WHERE ");
+		
+		_selectAllSources = connection.prepareStatement(sb.toString());
+		return _selectAllSources;
+	}
+	
+	private PreparedStatement getSelectSourceByTopicStatement() throws SQLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT * FROM ");
+		sb.append(DbConstants.SOURCE_TABLE);
+		sb.append(" WHERE ");
+		
+		_selectSourcesByTopic = connection.prepareStatement(sb.toString());
+		return _selectSourcesByTopic;
 	}
 }
